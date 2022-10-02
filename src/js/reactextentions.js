2afe38ffdc4a474a9a455687ae0ddd5d2e915f64
Sectimus <Sectimus@hotmail.co.uk>
2022-10-02 01:32:59 +0100

    added override for useState on react to add a callaback

---
+++ src/js/reactextentions.js
@@ -0,0 +1,23 @@
+import {useState as reactUseState, useEffect, useRef, useCallback } from 'react';
+function useState(initialState) {
+    const [state, setState] = reactUseState(initialState);
+    const cbRef = useRef(null); // init mutable ref container for callbacks
+  
+    const setStateCallback = useCallback((state, cb) => {
+      cbRef.current = cb; // store current, passed callback in ref
+      setState(state);
+    }, []); // keep object reference stable, exactly like `useState`
+  
+    useEffect(() => {
+      // cb.current is `null` on initial render, 
+      // so we only invoke callback on state *updates*
+      if (cbRef.current) {
+        cbRef.current(state);
+        cbRef.current = null; // reset callback after execution
+      }
+    }, [state]);
+  
+    return [state, setStateCallback];
+}
+
+export default useState;
